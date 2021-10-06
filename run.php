9987a7bc3a0fe76d6c326c7c1365170f3921370e
rsx0806 <42129362+rsx0806@users.noreply.github.com>
2021-10-06 12:09:30 +0800

    Update run.php

--- run.php
+++ run.php
@@ -3,27 +3,26 @@
 $argsCount = count($argv)-1;
 
 if( $argsCount != 2 ){
-    echo "2 arguments required\n";
+    echo "2 numeric arguments required\n";
     exit;
 }
 if( $argv[1] > $argv[2] ){
     echo "First argument must be less than the second\n";
     exit;
 }
-else{
-    for($i = $argv[1]; $i<= $argv[2]; $i++){
-        $output = "";
-        if( $i % 3 == 0 ){
-            $output .="Foo";
-        }
-        if( $i % 5 == 0 ){
-            $output .="Bar";
-        }
-        if(($i % 3 != 0) && ($i % 5 != 0)){
-            $output .=$i;
-        }
+for($i = $argv[1]; $i<= $argv[2]; $i++){
+    $output = "";
+    if( $i % 3 == 0 ){
+        $output .="Foo";
+    }
+    if( $i % 5 == 0 ){
+        $output .="Bar";
+    }
+    if(!empty($output)){
         echo $output."\n";
     }
+    else{
+        echo $i."\n";
+    }   
 }
-
 ?>
